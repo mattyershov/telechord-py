e1b48dae8a879ceb9aff048044d3d1c50fd97b7e
mattyershov <mattyershov@gmail.com>
2026-03-08 23:03:32 -0400

    added png encode attempt from earlier and kept working on binary encoding

--- src/chordencode.py
+++ src/chordencode.py
@@ -1,37 +1,50 @@
 import scipy
 import numpy as np
 
-rate = 44100 # sample rate
-duration = 0.3 # duration of tone in seconds
-freq1 = 300
-freq2 = 350
-freq3 = 400
-freq4 = 450
-freq5 = 500
-freq6 = 550
-freq7 = 600
-freq8 = 650
+# Old hardcoded frequencies
+# freq1:float=300,
+# freq2:float=350,
+# freq3:float=350,
+# freq4:float=400,
+# freq5:float=450,
+# freq6:float=500,
+# freq7:float=550,
+# freq8:float=600
 
-num_samples = max(1, int(rate * duration)) # ensures a non-empty function
-t = np.linspace(0, num_samples / rate, num_samples, endpoint=False)
+data = [1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1] # length must be a multiple of 8, otherwise data will be cut off
 
-data = [1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1] # length must be a multiple of 8
-temp = []
-split_data = [][]
+def format_data(data:list):
+    temp = []
+    data_split = []
+    
+    if (len(data) % 8 != 0):
+        print("WARNING: Length of binary data is not a multiple of 8! Data will be cut off!")
+    for i in range(0, len(data) // 8):
+        for j in range(8 * i, 8 * i + 8):
+            temp.append(data[j])
+        data_split.append(temp)
+        temp = []
+    print(data_split)
+    return data_split
 
-count = 1
-for i in range(0, len(data) / 8):
-    temp.append(i)
-    split_data.append(temp)
+def encode_chord(data:list, spacing:int, start_af:int=300, rate:int=44100, duration:float=0.3):
+    num_samples = max(1, int(rate * duration)) # ensures a non-empty function
+    t = np.linspace(0, num_samples / rate, num_samples, endpoint=False)
 
+    car1 = (np.sin(2 * np.pi * (start_af + spacing*1) * t)).astype(np.float32)
+    car2 = (np.sin(2 * np.pi * (start_af + spacing*2) * t)).astype(np.float32)
+    car3 = (np.sin(2 * np.pi * (start_af + spacing*3) * t)).astype(np.float32)
+    car4 = (np.sin(2 * np.pi * (start_af + spacing*4) * t)).astype(np.float32)
+    car5 = (np.sin(2 * np.pi * (start_af + spacing*5) * t)).astype(np.float32)
+    car6 = (np.sin(2 * np.pi * (start_af + spacing*6) * t)).astype(np.float32)
+    car7 = (np.sin(2 * np.pi * (start_af + spacing*7) * t)).astype(np.float32)
+    car8 = (np.sin(2 * np.pi * (start_af + spacing*8) * t)).astype(np.float32)
 
+    data = car1 + car2 + car3 + car4 + car5 + car6 + car7 + car8
+    scipy.io.wavfile.write("outputs/chord.wav", rate, data)
 
-car1 = (np.sin(2 * np.pi * freq1 * t)).astype(np.float32)
-car2 = (np.sin(2 * np.pi * freq2 * t)).astype(np.float32)
-car3 = (np.sin(2 * np.pi * freq3 * t)).astype(np.float32)
-car4 = (np.sin(2 * np.pi * freq4 * t)).astype(np.float32)
-car5 = (np.sin(2 * np.pi * freq5 * t)).astype(np.float32)
-car6 = (np.sin(2 * np.pi * freq6 * t)).astype(np.float32)
+def main():
+    format_data(data)
 
-data = car1 + car2 + car3 + car4 + car5 + car6
-scipy.io.wavfile.write("outputs/chord.wav", rate, data)
+if __name__=="__main__":
+    main()
---
+++ src/pngencode.py
@@ -0,0 +1,13 @@
+# UNFINISHED
+
+
+import base64
+
+def main():
+    with open("inputs/SMPTE_Color_Bars.png", "rb") as f:
+        img_data = f.read()
+
+    print(img_data)
+
+if (__name__ == "__main__"):
+    main()
